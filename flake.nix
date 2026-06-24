a242dc9032eae08282cabb85b4e1091886df325d
Marc Zander <marc.zander@siemens.com>
2026-06-24 11:23:11 +0000

    chore: update nixpkgs to 26.05, home-manager to 26.05

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "Private NixOS host configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     nixos-profiles.url = "git+https://codeberg.org/m4rc2a/nixos-profiles";
 
     disko.url = "github:nix-community/disko";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
