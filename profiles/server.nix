0dba6049eaa00b5cbda7b4857ae15940b907c3bb
Marc Zander <m4rc2a.opensource@tutanota.de>
2026-07-31 17:32:56 +0000

    chore: sort server profiles imports

--- profiles/server.nix
+++ profiles/server.nix
@@ -11,21 +11,25 @@
     ../modules/tools/files.nix
     ../modules/tools/processes.nix
     ../modules/tools/networking.nix
-    ../modules/tools/tmux.nix
     ../modules/tools/hardware.nix
     ../modules/tools/helix.nix
     ../modules/tools/archives.nix
     ../modules/tools/data.nix
 
     # Services
+    ## web
+    ../modules/services/nginx.nix
+
     ../modules/services/homeassistant.nix
-    ../modules/services/i2p.nix
     ../modules/services/invidious.nix
     ../modules/services/jellyfin.nix
-    ../modules/services/matter-server.nix
-    ../modules/services/nginx.nix
-    ../modules/services/openssh.nix
+
+    ../modules/services/i2p.nix
     ../modules/services/radarr.nix
+
+    ../modules/services/matter-server.nix
+
+    ../modules/services/openssh.nix
     ../modules/services/ssh-reverse-tunnel.nix
   ];
 
